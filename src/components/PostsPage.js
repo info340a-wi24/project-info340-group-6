b7315d33ce798eac05034d02a8be8bdb2252ae2f
bhs28 <bhs28@uw.edu>
2024-03-01 18:15:16 -0800

    No more images, because bad

--- src/components/PostsPage.js
+++ src/components/PostsPage.js
@@ -20,7 +20,7 @@ function Post(props) {
 
     return (
         <div className="post col-sm-6 col-md-4">
-            <img className='py-1' src={process.env.PUBLIC_URL + fire.source} alt={fire.alt} />
+            {/*<img className='py-1' src={process.env.PUBLIC_URL + fire.source} alt={fire.alt} />*/}
             <h2>{fire.header}</h2>
             <em>{fire.date} - {fire.coordinates}</em>
             <p>{fire.content}</p>
